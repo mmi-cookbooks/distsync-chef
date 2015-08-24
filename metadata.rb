63d59ae4dde1799e07b9c77383ac70b8dd7f1cdd
Michael Burns <michael.burns@rackspace.com>
2015-08-24 15:06:38 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "ele-dev@lists.rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures distsync"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 depends          "runit"
