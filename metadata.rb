6eebf452a52e00cf1af9745f96eca555db589b48
Michael Burns <michael@mirwin.net>
2018-02-18 20:25:09 -0800

    FC066: Ensure chef_version is set in metadata FC069: Ensure standardized license defined in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
 name 'distsync'
 maintainer 'Rackspace'
 maintainer_email 'ele-dev@lists.rackspace.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures distsync'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.2'
+chef_version '>= 12.7' if respond_to?(:chef_version)
+
 depends 'runit'
 
 source_url 'https://github.com/mmi-cookbooks/distsync-chef' if respond_to?(:source_url)
