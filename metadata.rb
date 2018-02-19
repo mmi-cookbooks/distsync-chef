f834e610f8dc5fe96e725a82b2b21a2b36d83a05
Michael Burns <michael@mirwin.net>
2018-02-18 20:25:42 -0800

    add Rake tasks

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ele-dev@lists.rackspace.com'
 license 'Apache-2.0'
 description 'Installs/Configures distsync'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.2.0'
 chef_version '>= 12.7' if respond_to?(:chef_version)
 
 depends 'runit'
